3e624374f258b804ff260d68bdfbce05b98b589e
HelixFix <jerome.rinner@gmail.com>
2023-04-10 23:45:00 +0200

    Refactor applyGrayToArchivedRepos function

--- content.js
+++ content.js
@@ -1,39 +1,27 @@
-const cache        = getRepoCache();
-const starredRepos = getStarredRepos();
-
 // fonction principale qui effectue le traitement sur les dépôts archivés.
 async function applyGrayToArchivedRepos() {
-  
+    let cache        = getRepoCache();
+    let starredRepos = getStarredRepos();
   for (const element of starredRepos) {
-    const repoName = element.querySelector("h3 a");
-
+    let repoName = getRepoName(element);
     if (repoName) {
-      const repoUrl   = repoName.href;
-      const cacheData = cache[repoUrl];
+      let repoUrl = repoName.href;
 
-      if (cacheData && isCacheValid(cacheData)) {
-        if (cacheData.archived) {
-          applyGrayToRepoName(repoName);
-        }
-
+      if (isRepoArchived(repoUrl, cache)) {
+        repoName.style.color = "gray";
+      } else {
         try {
           const response = await fetch(repoUrl);
-          const html     = await response.text();
+          const html = await response.text();
 
-          const doc       = new DOMParser().parseFromString(html, "text/html");
-          const flashWarn = doc.querySelector(
-            "div.flash.flash-warn.flash-full.border-top-0.text-center.text-bold.py-2"
-          );
+          let isArchived = checkIfRepoArchived(html);
 
-          if (flashWarn) {
-            applyGrayToRepoName(repoName);
-            updateRepoCache(repoUrl, true);
-          } else {
-            updateRepoCache(repoUrl, false);
+          if (isArchived) {
+            repoName.style.color = "gray";
           }
-        } catch (error) {
-          console.log(error);
-        }
+
+          updateRepoCache(repoUrl, isArchived, cache);
+        } catch (error) {}
       }
     }
   }
@@ -51,31 +39,33 @@ function getStarredRepos() {
   );
 }
 
-// Vérifie si le dépôt est en cache et si sa date de dernière mise à jour est antérieure à 7 jours.
-function isCacheValid(cacheData) {
-  const cacheDate            = new Date(cacheData.lastChecked);
-  const daysSinceLastChecked = (new Date() - cacheDate) / (24 * 60 * 60 * 1000);
-
-  return daysSinceLastChecked < 7;
+function getRepoName(element) {
+  return element.querySelector("h3 a");
 }
 
-// Applique un style "gris" au nom du dépôt.
-function applyGrayToRepoName(repoName) {
-  repoName.style.color = "gray";
+function isRepoArchived(repoUrl, cache) {
+  return (
+    cache[repoUrl] &&
+    new Date() - new Date(cache[repoUrl].lastChecked) <
+      7 * 24 * 60 * 60 * 1000 &&
+    cache[repoUrl].archived
+  );
 }
 
-// fonction qui parse une chaîne HTML en un document HTML.
-function parseHtml(html) {
-  const parser = new DOMParser();
-  return parser.parseFromString(html, "text/html");
+function checkIfRepoArchived(html) {
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(html, "text/html");
+  let flashWarn = doc.querySelector(
+    "div.flash.flash-warn.flash-full.border-top-0.text-center.text-bold.py-2"
+  );
+  return !!flashWarn;
 }
 
 // fonction qui met à jour le cache pour un dépôt donné.
-function updateRepoCache(repoUrl, archived) {
-  const cache = getRepoCache();
+function updateRepoCache(repoUrl, isArchived, cache) {
   cache[repoUrl] = {
     lastChecked: new Date().toISOString(),
-    archived: archived,
+    archived: isArchived,
   };
   localStorage.setItem("repoCache", JSON.stringify(cache));
 }
@@ -99,7 +89,7 @@ function checkFunctionActivation() {
 // Lorsque l'utilisateur clique, isFunctionActivated est remis à 0 et la fonction est appelée avec setInterval pour recommencer le processus.
 window.addEventListener("click", () => {
   isFunctionActivated = 0;
-  timerId             = setInterval(checkFunctionActivation, 2000);
+  timerId = setInterval(checkFunctionActivation, 2000);
 });
 
 timerId = setInterval(checkFunctionActivation, 2000);
